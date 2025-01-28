47404b9eb18ce501e3c8db061bb12f8506c18625
P.Adi Narayana <adipodili268@gmail.com>
2025-01-28 12:53:49 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,8 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int a = 8;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(a + " * " + i + " = " + (a * i));
+        }
+    }
+}
